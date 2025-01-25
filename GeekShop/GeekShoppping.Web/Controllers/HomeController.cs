8b9821c84a0e376cddd29766c5d0b354bd5b3fd1
Gustavo Lima <glima.0913@gmail.com>
2025-01-25 20:40:47 -0300

    feat: projeto eshop criado

---
+++ GeekShop/GeekShoppping.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using GeekShoppping.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShoppping.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
